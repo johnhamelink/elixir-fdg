a97a5bec07330de64324d49a4027ea1dab50b364
John Hamelink <john@johnhamelink.com>
2015-11-29 01:18:14 +0000

    Added ability to export graphviz dot syntax to images.

--- lib/fdg/parser.ex
+++ lib/fdg/parser.ex
@@ -1,6 +1,7 @@
 defmodule FDG.Parser do
   @moduledoc ~S"""
-  Parses FDG AST into Graphviz (.dot) syntax
+  Parses FDG AST into Graphviz (.dot) syntax, and allows
+  for exporting to Various image formats using Graphviz.
 
   ## Examples
 
@@ -10,9 +11,70 @@ defmodule FDG.Parser do
   """
 
   @typedoc """
-  Node tuple
+  Node tuple used to describe individual nodes. It's children is
+  a list of zero or more node_tuples.
   """
-  @type node_tuple :: {:node, [label: String.t, children: [...]]}
+  @type node_tuple :: {:node, [label: String.t, children: [node_tuple]]}
+
+  @doc ~S"""
+  Turns FDG `ast` tuples into a PNG digraph, using `Graphviz's`
+  `dot` command.
+
+  ## Examples
+
+      iex> FDG.Parser.to_dot([{:node, [label: "A", children: []]}])
+      ...> |> FDG.Parser.to_png("graph")
+      {:ok, "graph.png"}
+
+  """
+  @spec to_png(String.t, String.t) :: {atom(), String.t}
+  def to_png(dot_syntax, output_filename) do
+    dot_syntax
+    |> to_graphviz("png", output_filename)
+  end
+
+  @doc ~S"""
+  Turns FDG `ast` tuples into an SVG digraph, using `Graphviz's`
+  `dot` command.
+
+  ## Examples
+
+      iex> FDG.Parser.to_dot([{:node, [label: "A", children: []]}])
+      ...> |> FDG.Parser.to_svg("graph")
+      {:ok, "graph.svg"}
+
+  """
+  @spec to_svg(String.t, String.t) :: {atom(), String.t}
+  def to_svg(dot_syntax, output_filename) do
+    dot_syntax
+    |> to_graphviz("svg", output_filename)
+  end
+
+  @doc ~S"""
+  Turns dot syntax into something else using Graphviz. The `format`
+  attribute can accept anything that Graphviz can. A non-exhausted
+  list being:
+
+    - `ps` (PostScript)
+    - `svg` and `svgz` (Structured Vector Graphics)
+    - `fig` (XFIG Graphics)
+    - `png` and `gif` (Bitmap Graphics)
+    - `imap` and `cmapx`
+
+  ## Examples
+
+      iex> FDG.Parser.to_dot([{:node, [label: "A", children: []]}])
+      ...> |> FDG.Parser.to_graphviz("ps", "graph")
+      {:ok, "graph.ps"}
+
+  """
+  def to_graphviz(dot_syntax, format, output_filename) do
+    {:ok, tmp_file} = create_tmp_file(dot_syntax)
+    run_dot(tmp_file, output_filename, format)
+    delete_tmp_file(tmp_file)
+    {:ok, "#{output_filename}.#{format}"}
+  end
+
 
   @doc ~S"""
   Turns FDG `ast` tuples into a dot-syntax string, ready to
@@ -79,5 +141,22 @@ defmodule FDG.Parser do
       ] |> Enum.join(" ")
     end)
   end
+
+  @spec create_tmp_file(String.t) :: {:ok, String.t}
+  defp create_tmp_file(dot_syntax) do
+    tmp_file = "tmp.#{:os.system_time(:seconds)}.dot"
+    {:ok, file} = File.open tmp_file, [:write]
+    :ok = IO.binwrite file, dot_syntax
+    :ok = File.close file
+    {:ok, tmp_file}
+  end
+
+  @spec run_dot(String.t, String.t, String.t) :: {Collectable.t, exit_status :: non_neg_integer}
+  defp run_dot(input_file, output_filename, format) do
+    System.cmd("dot", ["-T#{format}", "-o#{output_filename}.#{format}", input_file])
+  end
+
+  @spec delete_tmp_file(String.t) :: :ok
+  defp delete_tmp_file(tmp_file), do: File.rm(tmp_file)
 
 end
--- test/fdg/parser_test.exs
+++ test/fdg/parser_test.exs
@@ -2,6 +2,26 @@ defmodule FDG.ParserTest do
   use ExSpec, async: true
   doctest FDG.Parser
 
+  setup do
+
+    on_exit fn ->
+      # Delete all the test files
+      Enum.each(["png", "svg", "ps"], fn(fmt) ->
+        File.rm "graph.#{fmt}"
+      end)
+    end
+
+    :ok
+  end
+
+
+  @complex_ast [
+    {:node, [label: "A", children: [
+      {:node, [label: "B", children: []]},
+      {:node, [label: "C", children: []]}
+    ]]}
+  ]
+
   it "Creates hash from label, and adds label to node" do
     ast = [{:node, [label: "Lorem Ipsum", children: []]}]
     dot_syntax = "digraph G { lorem_ipsum [label=\"Lorem Ipsum\"]; }"
@@ -9,15 +29,20 @@ defmodule FDG.ParserTest do
   end
 
   it "Parses Tuples into dot syntax recursively" do
-    ast = [
-      {:node, [label: "A", children: [
-        {:node, [label: "B", children: []]},
-        {:node, [label: "C", children: []]}
-      ]]}
-    ]
-
     dot_syntax = "digraph G { a [label=\"A\"]; b [label=\"B\"]; c [label=\"C\"]; a -> b; a -> c; }"
-    assert FDG.Parser.to_dot(ast) == dot_syntax
+    assert FDG.Parser.to_dot(@complex_ast) == dot_syntax
+  end
+
+  it "Can produce SVG files using Graphviz's `dot` command" do
+    assert {:ok, "graph.svg"} = FDG.Parser.to_dot(@complex_ast)
+                                |> FDG.Parser.to_svg("graph")
+    assert File.exists? "graph.svg"
+  end
+
+  it "Can produce PNG files using Graphviz's `dot` command" do
+    assert {:ok, "graph.png"} = FDG.Parser.to_dot(@complex_ast)
+                                |> FDG.Parser.to_png("graph")
+    assert File.exists? "graph.png"
   end
 
 end
