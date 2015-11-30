26f94d6c833796052100af1ff8f4e6928f4d0641
John Hamelink <john@johnhamelink.com>
2015-11-30 19:37:18 +0000

    Built unit test with mock implementation

--- lib/fdg/importers/cypher.ex
+++ lib/fdg/importers/cypher.ex
@@ -1,2 +1,15 @@
 defmodule FDG.Importers.Cypher do
+
+  def parse(cypher) when is_bitstring(cypher) do
+    [
+      {:node, [
+          label: "A", children: [
+            {:node, [label: "B", children: []]},
+            {:node, [label: "C", children: []]}
+          ]
+        ]
+      }
+    ]
+  end
+
 end
--- test/fdg/importers/cypher_test.exs
+++ test/fdg/importers/cypher_test.exs
@@ -1,30 +1,23 @@
 defmodule FDG.Importers.CypherTest do
-  require IEx
   use ExSpec, async: true
   doctest FDG.Importers.Cypher
 
-  # Connect to the Neo4j Database
-  @neo :neo4j.connect([{:base_uri, 'http://localhost:7474/db/data/'}])
+  @simple [
+    cypher: ~S"""
+      begin
+        CREATE (a:Root {name: 'A'}), (b:Child {name: 'B'}), (c:Child {name: 'C'});
+        MATCH (a:Root),(b:Child) CREATE (a)-[r:CHILD_OF]->(b)";
+      commit
+    """,
+    ast: [
+      {:node, [label: "A", children: [
+        {:node, [label: "B", children: []]},
+        {:node, [label: "C", children: []]}
+      ]]}
+    ]
+  ]
 
-  setup do
-    # Create some nodes & Relationships
-    nodes_cypher = <<"CREATE (a:Root {name: 'A'}), (b:Child {name: 'B'}), (c:Child {name: 'C'})">>
-    rels_cypher = <<"MATCH (a:Root),(b:Child) CREATE (a)-[r:CHILD_OF]->(b)">>
-    :neo4j.cypher(@neo, nodes_cypher)
-    :neo4j.cypher(@neo, rels_cypher)
-
-    on_exit fn ->
-      # Delete the root node & all its children
-      :neo4j.cypher(@neo, <<"MATCH (a:Root)-[b]-(c) DELETE a,b,c">>)
-    end
+  it "converts CYPHER into our AST" do
+    assert FDG.Importers.Cypher.parse(@simple[:cypher]) == @simple[:ast]
   end
-
-
-  it "understands nodes" do
-    {graph} = :neo4j.cypher(@neo, <<"MATCH (a:Root)-[b]-(c) return a,b,c">>)
-    {"columns", columns} = graph |> List.first
-    {"data", data}       = graph |> List.last
-    IEx.pry
-  end
-
 end
